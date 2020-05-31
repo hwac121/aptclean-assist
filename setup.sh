728b3b68b7389910dc5dc312d2cd064e735974be
hwac121 <hwac121@protonmail.com>
2020-05-31 17:02:07 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo " _______  _______ _________ _______  _        _______  _______  _"
+echo "(  ___  )(  ____ )\__   __/(  ____ \( \      (  ____ \(  ___  )( (    /|"
+echo "| (   ) || (    )|   ) (   | (    \/| (      | (    \/| (   ) ||  \  ( |"
+echo "| (___) || (____)|   | |   | |      | |      | (__    | (___) ||   \ | |"
+echo "|  ___  ||  _____)   | |   | |      | |      |  __)   |  ___  || (\ \) |"
+echo "| (   ) || (         | |   | |      | |      | (      | (   ) || | \   |"
+echo "| )   ( || )         | |   | (____/\| (____/\| (____/\| )   ( || )  \  |"
+echo "|/     \||/          )_(   (_______/(_______/(_______/|/     \||/    )_)"
+echo " "
+echo " _______  _______  _______ _________ _______ _________"
+echo "(  ___  )(  ____ \(  ____ \\__   __/(  ____ \\__   __/"       
+echo "| (   ) || (    \/| (    \/   ) (   | (    \/   ) ("
+echo "| (___) || (_____ | (_____    | |   | (_____    | |"
+echo "|  ___  |(_____  )(_____  )   | |   (_____  )   | |"
+echo "| (   ) |      ) |      ) |   | |         ) |   | |"
+echo "| )   ( |/\____) |/\____) |___) (___/\____) |   | |"
+echo "|/     \|\_______)\_______)\_______/\_______)   )_("
+echo " "
+echo "ASSISTANT SCRIPT FOR aptsources-cleanup BY David Foerster"
+sleep 3
+echo " "
+
+#####################################
+#            MAIN SCRIPT            #
+#####################################
+
+echo "Installing quick shortcut for aptsources-cleanup by David Foerster"
+sleep 1
+echo "You can find him on github as davidfoerster"
+sleep 1
+cp ac.data /usr/bin/aptclean
+echo "To activate aptsources-cleanup simply type 'aptclean' anywhere in a terminal."
+sleep 1
+echo "Installation complete!"
